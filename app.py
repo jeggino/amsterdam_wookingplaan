0f7d3d99eba013137f4a649cf669315f9e1698eb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-04-03 09:56:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -48,22 +48,19 @@ selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'],
     }
 )
 
+with st.sidebar:
+    filter_year = st.slider("Kies jaarreeks", int(df.Start_bouw.min()), int(df.Start_bouw.max()), 
+                        value=(int(df.Start_bouw.min()),
+                               int(df.Start_bouw.max()))
+                       )
+    filter_fase = st.multiselect('Kies wat voor soort bouwfase',['Investeringsbesluit genomen','In aanbouw genomen','Verkenning','Principebesluit genomen'])
+        
 if selected3 == "Home":
-    # -------------------------------------------------------
-    with st.sidebar:
-        filter_year = st.slider("Kies jaarreeks", int(df.Start_bouw.min()), int(df.Start_bouw.max()), 
-                            value=(int(df.Start_bouw.min()),
-                                   int(df.Start_bouw.max()))
-                           )
-        filter_fase = st.multiselect('Kies wat voor soort bouwfase',['Investeringsbesluit genomen','In aanbouw genomen','Verkenning','Principebesluit genomen'])
-
-
+   
     # -------------------------------------------------------
     choices_bouw = (df.Start_bouw>=filter_year[0]) & (df.Start_bouw<=filter_year[1])
     choices_fase = (df.Fase.isin(filter_fase))
     df_filter = df[choices_bouw & choices_fase]
-
-    # with st.sidebar:
 
     left, right = st.columns([3,2])
 
@@ -93,63 +90,63 @@ if selected3 == "Home":
 
 
 elif selected3 == "Upload":
-# -------------------------------------------------------
-with st.container():
+    # -------------------------------------------------------
+    with st.container():
 
-    filter_rent = st.selectbox('Kies wat voor soort huur',('Dure_huur','Sociale_huur','Middeldure_huur', 'Dure_huur_of_Koop','Koop'))
+        filter_rent = st.selectbox('Kies wat voor soort huur',('Dure_huur','Sociale_huur','Middeldure_huur', 'Dure_huur_of_Koop','Koop'))
 
-    INITIAL_VIEW_STATE = pdk.ViewState(
-        latitude=52.374119, 
-        longitude=4.895906,
-        zoom=10,
-        pitch=45,
-        bearing=0
-    )
+        INITIAL_VIEW_STATE = pdk.ViewState(
+            latitude=52.374119, 
+            longitude=4.895906,
+            zoom=10,
+            pitch=45,
+            bearing=0
+        )
 
-    COLOR_RANGE = [
-        [255, 255, 204],
-        [254, 217, 118],
-        [253, 141, 60],
-        [128, 0, 38],
-        [90, 0, 25],
-        [50, 0, 15]
-    ]
+        COLOR_RANGE = [
+            [255, 255, 204],
+            [254, 217, 118],
+            [253, 141, 60],
+            [128, 0, 38],
+            [90, 0, 25],
+            [50, 0, 15]
+        ]
 
-    BREAKS = [(df_filter[filter_rent].max()*1)/6,
-              (df_filter[filter_rent].max()*2)/6,
-              (df_filter[filter_rent].max()*3)/6,
-              (df_filter[filter_rent].max()*4)/6,
-              (df_filter[filter_rent].max()*5)/6,
-              df_filter[filter_rent].max()/6,]
+        BREAKS = [(df_filter[filter_rent].max()*1)/6,
+                  (df_filter[filter_rent].max()*2)/6,
+                  (df_filter[filter_rent].max()*3)/6,
+                  (df_filter[filter_rent].max()*4)/6,
+                  (df_filter[filter_rent].max()*5)/6,
+                  df_filter[filter_rent].max()/6,]
 
 
-    def color_scale(val):
-        for i, b in enumerate(BREAKS):
-            if val < b:
-                return COLOR_RANGE[i]
-        return COLOR_RANGE[i]
+        def color_scale(val):
+            for i, b in enumerate(BREAKS):
+                if val < b:
+                    return COLOR_RANGE[i]
+            return COLOR_RANGE[i]
 
-    df_filter["color"] = df_filter[filter_rent].apply(lambda x: color_scale(x))
+        df_filter["color"] = df_filter[filter_rent].apply(lambda x: color_scale(x))
 
-    polygon_layer = pdk.Layer(
-        'GeoJsonLayer',
-        df_filter,
-        opacity=0.6,
-        stroked=True,
-        filled=True,
-        extruded=True,
-        wireframe=True,
-        get_elevation=filter_rent,
-        get_fill_color='color',
-        get_line_color=[255, 255, 255],
-        pickable=True
-    )
+        polygon_layer = pdk.Layer(
+            'GeoJsonLayer',
+            df_filter,
+            opacity=0.6,
+            stroked=True,
+            filled=True,
+            extruded=True,
+            wireframe=True,
+            get_elevation=filter_rent,
+            get_fill_color='color',
+            get_line_color=[255, 255, 255],
+            pickable=True
+        )
 
-    r = pdk.Deck(
-        [polygon_layer],
-        tooltip = {"text": f"Number of {filter_rent}: {filter_rent}"},
-        map_style = "light",
-        initial_view_state=INITIAL_VIEW_STATE,
-    )
+        r = pdk.Deck(
+            [polygon_layer],
+            tooltip = {"text": f"Number of {filter_rent}: {filter_rent}"},
+            map_style = "light",
+            initial_view_state=INITIAL_VIEW_STATE,
+        )
 
-    st.pydeck_chart(pydeck_obj=r, use_container_width=True)
+        st.pydeck_chart(pydeck_obj=r, use_container_width=True)
