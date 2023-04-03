4e0b145d5d1ae8eff3ba8cf0b22000cdf4eb148f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-04-03 10:04:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,20 +33,13 @@ def get_data():
     df_raw = df_raw[df_raw.Start_bouw!=0]
     return df_raw
 
+# -------------------------------------------------------
+selected3 = option_menu(None, ["Charts", "Kaart"], 
+    icons=['bi bi-clipboard2-data', 'bi bi-map'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+
+# -------------------------------------------------------
 df = get_data()
-
-
-# 3. CSS style definitions
-selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal",
-    styles={
-        "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "green"},
-    }
-)
 
 with st.sidebar:
     filter_year = st.slider("Kies jaarreeks", int(df.Start_bouw.min()), int(df.Start_bouw.max()), 
