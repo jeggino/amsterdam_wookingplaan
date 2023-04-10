d5bc1338d11a6a3aec1b0dea4fba270a158e58a7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-04-10 10:07:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -157,8 +157,8 @@ for i in df_metrics.columns:
                         }
 #-------------------------
 
-filter_rent = selectbox('Kies wat voor soort huur',('Dure_huur','Sociale_huur','Middeldure_huur', 'Dure_huur_of_Koop','Koop'))
-filter_map = selectbox('',('road', 'light_no_labels', 'dark_no_labels'),label_visibility="collapsed")
+filter_rent = st.selectbox('Kies wat voor soort huur',('Dure_huur','Sociale_huur','Middeldure_huur', 'Dure_huur_of_Koop','Koop'))
+filter_map = st.selectbox('',('road', 'light_no_labels', 'dark_no_labels'),label_visibility="collapsed")
 
 
 INITIAL_VIEW_STATE = pdk.ViewState(
