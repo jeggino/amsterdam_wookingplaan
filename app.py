3cbcad8ddacecddb5a32eb7e895ae6f06589e529
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-04-11 07:40:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -254,8 +254,15 @@ r = pdk.Deck(
 map_right.pydeck_chart(pydeck_obj=r, use_container_width=True)
 
     
-
-        
+#--------------------------------------------------
+import plotly.express as px
+import numpy as np
+df = px.data.gapminder().query("year == 2007")
+fig = px.sunburst(df, path=['continent', 'country'], values='pop',
+                  color='lifeExp', hover_data=['iso_alpha'],
+                  color_continuous_scale='RdBu',
+                  color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop']))
+st.plotly_chart(fig, use_container_width=False, sharing="streamlit", theme="streamlit")
 
 
 
