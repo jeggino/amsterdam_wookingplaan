445680a07783a0e533c94eb5e38c1998386d97e0
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-03 13:04:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,6 @@ if filter_genre == 'Totaal':
     df_timeseries = pd.melt(df_filter, id_vars=['Start_bouw'], 
            value_vars=['Sociale_huur', 'Middeldure_huur', 'Dure_huur', 'Dure_huur_of_Koop','Koop']) 
     #-------------------------
-    st.dataframe(df_filter.drop("geometry",axis=1))
     df_metrics = df_filter.groupby("Start_bouw")[['Sociale_huur', 'Middeldure_huur', 'Dure_huur', 'Dure_huur_of_Koop','Koop']].sum()
 
     #-------------------------
